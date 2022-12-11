cc8467dc8418ce659356bae43d7a59cc45e1e4d5
Marcelo Vergara <marcelorv@gmail.com>
2022-12-11 04:45:09 -0300

    change paramters

--- src/index.js
+++ src/index.js
@@ -45,12 +45,11 @@ venom
       logQR: false,
       disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
       disableWelcome: true, // Will disable the welcoming message which appears in the beginning
-      headless: true,
       puppeteerOptions: {
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        executablePath: "/usr/bin/chromium-browser",
       },
+      debug: true,
     }
   )
   .then((client) => start(client))
