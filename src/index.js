2b7634eec3c7f626260dd16990f8c324f96f6705
Marcelo Vergara <marcelorv@gmail.com>
2023-04-01 17:19:19 -0300

    incoming msg twilio 4

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.route("/messages").post(async (req, res) => {
 
 // receive whatsapp message
 app.route("/incoming").post(async (req, res) => {
-  console.log("req 2", client.numbers);
+  console.log("req 2", client.from);
   try {
     client.messages
       .create({
